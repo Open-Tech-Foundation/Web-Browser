e9ecaabd49c87cb9ddf3f8d38d9548de0f9960f9
Thanga Ganapathy <ganapathy888@gmail.com>
2026-05-14 12:11:22 +0530

    fix: simplify insecure blocked page

--- ui/src/security/main.jsx
+++ ui/src/security/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Security from './Security'
-import '../styles/App.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Security from './Security';
+import '../styles/App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Security />
   </React.StrictMode>,
-)
+);
